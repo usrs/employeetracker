b5fed2944fb49d0ef510faf67eb46e0c593bac94
nathan mc <nrsm@protonmail.ch>
2020-05-26 17:54:12 -0700

    created schema & seed files

---
+++ sql/seed.sql
@@ -0,0 +1,9 @@
+USE employee_db;
+
+INSERT INTO department (name)
+VALUES ('sales'), ('engineering'), ('marketing'), ('warehouse');
+
+INSERT INTO role (title, salary)
+VALUES ('executive', 150000), ('manager', 120000), ('engineer', 110000), ('sales-person', 80000);
+
+INSERT INTO employee (first_name, last_name)
